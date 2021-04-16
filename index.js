88b46c197b19ae347e4293b9a77c13885dd1c2a7
Michelle Berta <bertafamily@sbcglobal.net>
2021-04-16 17:29:11 -0500

    wrote function to run through inquirer and ask title

--- index.js
+++ index.js
@@ -1,69 +1,75 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
-const Manager = require('./lib/manager');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 
 const employees = [];
 
 function addEmplolyee() {
-    inquirer.prompt([{
-message: "What is the new employee's name?",
-name: "name"
-    },
-    
-    {
+  inquirer
+    .prompt([
+      {
+        message: "What is the new employee's name?",
+        name: "name",
+      },
+
+      {
         message: "What is the new employee's id?",
-        name: "id"
-    },
+        name: "id",
+      },
 
-    {
+      {
         message: "What is the new employee's email?",
-        name: "email"
-    },
+        name: "email",
+      },
 
-    {
+      {
         type: "list",
         message: "Please select the new employee's title",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager",
-        ], 
-        title: "title"
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "title",
+      },
+    ])
 
-    // how can this be added to inquire above?
-    {
-    then(function({name, title, id, email}) {
-        let titleInfo = '';
-        if (title === "Engineer") {
-            titleInfo = "gitHub username";
-        } else if (title === "Intern") {
-            titleInfo = "school name";
-        } else {
-            titleInfo === "office phone number";
-        }
+    // is this part of inquire above?
 
-    },
-    
-    )}
-    
-        
-    {
-        type: "list",
-        message: "Do you want to add another employee?", 
-        choices: [
-            "yes",
-            "no"
-        ],
-        // how do i start this function over again?
-        name: "moreEmployees"
-    }
+    .then(function ({ name, title, id, email }) {
+      let titleInfo = "";
+      let nameInfo = "";
+      if (title === "Engineer") {
+        titleInfo = "What is Engineer's gitHub username?";
+        nameInfo = "github";
+      } else if (title === "Intern") {
+        titleInfo = "What is the Intern's school name?";
+        nameInfo = "school";
+      } else {
+        titleInfo = "What is the Manager's office number?";
+        nameInfo = "officeNumber";
+      }
+      inquirer
+        .prompt([
+          {
+            message: titleInfo,
+            name: nameInfo,
+          },
+          {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to add another employee?',
+            default: false,
+          },
+        ])
+        .then(function (response) {
+            employees.push({name, title, id, email, ...response})
+            if (response.addEmployee) {
+               addEmplolyee() 
+            } else {
+                let titleInfo = "";
+          console.log(employees);
+            }
+        });
+    });
 }
 
-
-// need init function
-
-// need a function to write html
-
-// fs write/append file
+addEmplolyee();
